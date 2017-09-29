b6998f25bc459b48c4e7f968472db3a875d7ba31
László Agárdi <laszlo.agardi@tresorit.com>
2017-09-29 13:04:59 +0200

    Fix thread sanitizer simultaneous accesses warning

--- ZeroKit/Internal/Crypto.swift
+++ ZeroKit/Internal/Crypto.swift
@@ -2,15 +2,16 @@ import UIKit
 
 class Crypto {
     class func aesGcmEncrypt(plainData: Data, key: Data, iv: Data, adata: Data, tagLength: Int) -> Data? {
-        var cipherData = Data(count: plainData.count)
+        let cipherDataLength = plainData.count;
+        var cipherData = Data(count: cipherDataLength)
         var tag = Data(count: tagLength)
         
         func encrypt(keyPtr: UnsafePointer<UInt8>, ivPtr: UnsafePointer<UInt8>, adataPtr: UnsafePointer<UInt8>, plainDataPtr: UnsafePointer<UInt8>, cipherDataPtr: UnsafeMutablePointer<UInt8>, tagPtr: UnsafeMutablePointer<UInt8>) -> Int32 {
             switch key.count * 8 {
             case 128:
-                return encryptAes128GcmRaw(keyPtr, key.count, ivPtr, iv.count, adataPtr, adata.count, plainDataPtr, plainData.count, cipherDataPtr, cipherData.count, tagPtr, tag.count)
+                return encryptAes128GcmRaw(keyPtr, key.count, ivPtr, iv.count, adataPtr, adata.count, plainDataPtr, plainData.count, cipherDataPtr, cipherDataLength, tagPtr, tagLength)
             case 256:
-                return encryptAes256GcmRaw(keyPtr, key.count, ivPtr, iv.count, adataPtr, adata.count, plainDataPtr, plainData.count, cipherDataPtr, cipherData.count, tagPtr, tag.count)
+                return encryptAes256GcmRaw(keyPtr, key.count, ivPtr, iv.count, adataPtr, adata.count, plainDataPtr, plainData.count, cipherDataPtr, cipherDataLength, tagPtr, tagLength)
             default:
                 return -1
             }
@@ -43,14 +44,15 @@ class Crypto {
     class func aesGcmDecrypt(cipherData: Data, key: Data, iv: Data, adata: Data, tagLength: Int) -> Data? {
         let cipherBytes = cipherData.subdata(in: cipherData.startIndex ..< cipherData.endIndex.advanced(by: -tagLength))
         let tag = cipherData.subdata(in: cipherData.endIndex.advanced(by: -tagLength) ..< cipherData.endIndex)
-        var plainData = Data(count: cipherBytes.count)
+        let plainDataLength = cipherBytes.count
+        var plainData = Data(count: plainDataLength)
         
         func decrypt(keyPtr: UnsafePointer<UInt8>, ivPtr: UnsafePointer<UInt8>, adataPtr: UnsafePointer<UInt8>, cipherBytesPtr: UnsafePointer<UInt8>, tagPtr: UnsafePointer<UInt8>, plainDataPtr: UnsafeMutablePointer<UInt8>) -> Int32 {
             switch key.count * 8 {
             case 128:
-                return decryptAes128GcmRaw(keyPtr, key.count, ivPtr, iv.count, adataPtr, adata.count, cipherBytesPtr, cipherBytes.count, tagPtr, tag.count, plainDataPtr, plainData.count)
+                return decryptAes128GcmRaw(keyPtr, key.count, ivPtr, iv.count, adataPtr, adata.count, cipherBytesPtr, cipherBytes.count, tagPtr, tag.count, plainDataPtr, plainDataLength)
             case 256:
-                return decryptAes256GcmRaw(keyPtr, key.count, ivPtr, iv.count, adataPtr, adata.count, cipherBytesPtr, cipherBytes.count, tagPtr, tag.count, plainDataPtr, plainData.count)
+                return decryptAes256GcmRaw(keyPtr, key.count, ivPtr, iv.count, adataPtr, adata.count, cipherBytesPtr, cipherBytes.count, tagPtr, tag.count, plainDataPtr, plainDataLength)
             default:
                 return -1
             }
